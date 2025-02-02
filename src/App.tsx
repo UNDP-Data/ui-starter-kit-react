38d0505bb93c78f1ec417d57717275048baf2da2
Mustafa Saifee <mustafa.saifee@undp.org>
2025-02-03 00:26:13 +0200

    Add design system

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { H3 } from '@undp-data/undp-design-system-react';
+import '@undp-data/undp-design-system-react/dist/style.css';
 import undpLogo from './assets/undp-logo-blue.svg';
 
 function App() {
   return (
-    <div className='undp-container flex-div flex-wrap flex-hor-align-center margin-top-13 margin-bottom-13'>
+    <div>
       <div>
         <img
           src={undpLogo}
@@ -12,16 +14,13 @@ function App() {
           style={{ margin: 'auto' }}
         />
       </div>
-      <h3
-        className='undp-typography'
-        style={{ textAlign: 'center', width: '100%' }}
-      >
+      <H3 className='text-center'>
         This is template for building visualization and frontend project
         maintained by UNDP&apos;s SDG Integration Team.
         <br />
         <br />
         Contact us at data@undp.org if you have any feedback or questions.
-      </h3>
+      </H3>
     </div>
   );
 }
